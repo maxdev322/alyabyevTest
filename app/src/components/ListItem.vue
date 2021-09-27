1f7629c976b2120433957224d6b1f5ab09f1e634
Maxim Alyabyev <max3d99@mail.ru>
2021-09-27 13:55:27 +0300

    Добавлен список с данными и описан вывод данных в элементе списка

--- app/src/components/ListItem.vue
+++ app/src/components/ListItem.vue
@@ -1,8 +1,26 @@
-<template></template>
+<template>
+  <li class="list__item">
+    <strong>{{ id }}.</strong> {{ first_name }} {{ last_name }} <br />
+    {{ email }} <br />
+    Gender: {{ gender }}
+  </li>
+  <!-- <div class="kewhfb">+</div> , "first_name", "last_name", "email", "gender", "ip_address-->
+</template>
 
 <script>
-export default {};
+export default {
+  props: ["id", "first_name", "last_name", "email", "gender", "ip_address"],
+};
 </script>
 
 <style>
+.list__item {
+  border: 1px solid #000;
+  padding: 10px;
+  margin-bottom: 15px;
+  list-style-type: none;
+}
+.list__item strong {
+  font-size: 20px;
+}
 </style>
